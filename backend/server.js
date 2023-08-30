2948f176ed3e3484118f59f3a094d06d69860fd2
kyj91032 <sees77770@gmail.com>
2023-08-31 01:43:23 +0900

    nodemon 설치

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const chats = require('./data/data');
 
 const app = express();
 
@@ -6,6 +7,12 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.get
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+    console.log(req.params.id);
+});
 
 app.listen(5000, () => console.log('Server running on port 5000'));
