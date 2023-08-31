f41d6407d8992fd4fa8dcd3988c911e9feb00c3c
kyj91032 <sees77770@gmail.com>
2023-08-31 23:43:18 +0900

    .env 환경변수 설정

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const chats = require('./data/data');
 
 const app = express();
+dotenv.config(); // .env 파일에 있는 환경변수를 process.env에 넣어줌.
 
 app.get('/', (req, res) => {
     res.send('API is running...');
@@ -12,7 +14,11 @@ app.get('/api/chat', (req, res) => {
 });
 
 app.get('/api/chat/:id', (req, res) => {
-    console.log(req.params.id);
+    // console.log(req.params.id);
+    const singleChat = chats.find(c => c._id === req.params.id); // find 순환함수. c는 chats의 각각의 요소를 의미함. 요청 id와 같은 id를 가진 chat데이터를 찾음.
+    res.send(singleChat);
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 8080;
+
+app.listen(8080, () => console.log('Server running on port ${8080}'));
