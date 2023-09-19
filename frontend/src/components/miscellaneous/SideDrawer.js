7073b0e3747ca604b18e6e8b2c9dac186db106b5
kyj91032 <sees77770@gmail.com>
2023-09-19 20:24:09 +0900

    feat(design): ChatPage 디자인
    - 상단 바 추가
    - 검색 버튼 추가
    - 알람 버튼 추가

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,5 +1,8 @@
-import { Box, Tooltip } from '@chakra-ui/react';
+import { Box, Button, Text, Tooltip, Menu, MenuButton, MenuList } from '@chakra-ui/react';
 import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'; // 필요한 아이콘을 추가합니다.
+import { BellIcon } from '@chakra-ui/icons'
 
 const SideDrawer = () => {
 
@@ -9,19 +12,49 @@ const SideDrawer = () => {
     const [loadingChat, setLoadingChat] = useState(false);
 
     return <>
-        <Box>
-            <Tooltip
-                label='search Users to chat'
-                hasArrow
-                placement='bottem-end'
-            >
-                <Box>
-                    SideDrawer
-                </Box>
+        <Box
+            d='flex'
+            flexDirection='row'
+            justifyContent='space-between'
+            alignItems='center'
+            // bg='rgb(177, 197, 233)'
+            bg='gray.200'
+            w='100%'
+            p='5px 10px 5px 10px'
+            borderRadius='10px'
+        >
+            <Tooltip label='초대할 유저 검색' hasArrow placement='top'>
+                <Button variant='ghost' >
+                    <FontAwesomeIcon icon={faSearch} />
+                    <Text d={{base:'none', md:'flex'}} px='4'>
+                        유저 검색
+                    </Text>
+                </Button>
             </Tooltip>
+            
+            <Text display="inline" fontSize='2xl' fontFamily='Work sans'>
+                TCP-IP
+            </Text>
+            
+            <span>
+                <Menu>
+                    <MenuButton p={1}>
+                        <BellIcon fontSize='2xl' m={1}/>
+                    </MenuButton>
+                    {/* <MenuList>
+
+                    </MenuList> */}
+                </Menu>
+                <Menu>
+                    <MenuButton p={1}>
+                        <BellIcon fontSize='2xl' m={1}/>
+                    </MenuButton>
+                </Menu>
+            </span>
+            
         </Box>
     </>
     
 }
 
-export default SideDrawer;
+export default SideDrawer
--- frontend/src/pages/Chatpage.js
+++ frontend/src/pages/Chatpage.js
@@ -12,15 +12,9 @@ const Chatpage = () => {
   return (
     <div style={{width: '100%'}}>
       {user && <SideDrawer/>}
-      <Box
-        d='flex'
-        justifyContent={'space-between'}
-        w='100%'
-        h='91.5vh'
-        p='10px'
-      >
-        {user && <MyChats style={{ color: 'black' }}/>}
-        {user && <ChatBox/>}
+      <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+        {user && <MyChats />}
+        {user && <ChatBox />}
       </Box>
     </div>
   )
