3e0c50b9bcae1137e05ad23aa3d380ad2c9e9064
kyj91032 <sees77770@gmail.com>
2023-09-24 13:50:07 +0900

    feat(search): 유저 검색 api 연결

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -6,6 +6,10 @@ import { BellIcon, ChevronDownIcon } from '@chakra-ui/icons'
 import { ChatState } from "../../context/ChatProvider"
 import ProfileModal from './ProfileModal';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { set } from 'mongoose';
+import ChatLoading from '../ChatLoading';
+import UserListItem from '../UserAvatar/UserListItem';
 
 const SideDrawer = () => {
 
@@ -14,7 +18,7 @@ const SideDrawer = () => {
     const [loading, setLoading] = useState(false);
     const [loadingChat, setLoadingChat] = useState(false);
 
-    const { user } = ChatState();
+    const { user , setSelectedChat, chats, setChats} = ChatState();
 
     const navigate = useNavigate();
 
@@ -41,11 +45,58 @@ const SideDrawer = () => {
         }
 
         try {
-            
+            setLoading(true);
+
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${user.token}`
+                }
+            }
+            const {data} = await axios.get(`/api/user?search=${search}`, config);
+
+            setLoading(false);
+            setSearchResults(data);
         } catch (error) {
-            
+            toast({
+                title: '오류 발생.',
+                status: '검색 실패',
+                duration: 2000,
+                isClosable: true,
+                position: 'bottom-left',
+                colorScheme: 'blackAlpha'
+            });
         }
     }
+
+    const accessChat = async (userId) => {
+        try {
+            setLoadingChat(true);
+
+            const config = {
+                headers: {
+                    "Content-Type":"application/json",
+                    Authorization: `Bearer ${user.token}`
+                }
+            };
+
+            const {data} = await axios.post('/api/chat', {userId}, config);
+
+            setSelectedChat(data);
+            setLoadingChat(false);
+            onClose();
+        } catch (error) {
+            toast({
+                title: '채팅 불러오기 실패',
+                description: error.message,
+                status: 'error',
+                duration: 2000,
+                isClosable: true,
+                position: 'bottom-left',
+            });
+            
+        }
+    };
+
 
     return <>
         <Box
@@ -105,9 +156,19 @@ const SideDrawer = () => {
                         <Input placeholder='유저 이름 검색하기' mr={2} value={search} onChange={(e) => setSearch(e.target.value)}/>
                         <Button onClick={handleSearch}>검색</Button>
                     </Box>
+                    {loading ? (
+                        <ChatLoading/>
+                    ) : (
+                        searchResults?.map(user => (
+                            <UserListItem
+                                key={user._id}
+                                user={user}
+                                handleFucntion={()=>accessChat(user._id)}
+                            />
+                        ))
+                    )}
                 </DrawerBody>
             </DrawerContent>
-            
         </Drawer>
     </>
     
--- frontend/src/context/ChatProvider.js
+++ frontend/src/context/ChatProvider.js
@@ -8,7 +8,9 @@ const ChatProvider = ({ children }) => {
 
     const navigate = useNavigate();
 
-    const [user, setUser] = useState();
+    const [user, setUser] = useState(); // 로그인한 유저
+    const [selectedChat, setSelectedChat] = useState(); // 선택된 채팅방
+    const [chats, setChats] = useState(); // 채팅방 목록
   
     useEffect(() => {
         const userInfo = JSON.parse(localStorage.getItem("userInfo"));
@@ -21,7 +23,7 @@ const ChatProvider = ({ children }) => {
     }, [navigate]); // 경로 변경이 될 때마다 userInfo를 확인. userInfo가 없으면 홈으로 이동
 
     return ( // index.js에서 ChatProvider로 감싸진 모든 컴포넌트들이 children이 된다.
-        <ChatContext.Provider value={{ user, setUser }}>
+        <ChatContext.Provider value={{ user, setUser, selectedChat, setSelectedChat, chats, setChats }}>
             {children}
         </ChatContext.Provider>
     )
